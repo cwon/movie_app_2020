f606c5f3a0daa70f9994480016c327c8e49531e4
Won cheehun <dream40@nate.com>
2020-07-17 19:50:33 +0900

    Updates

---
+++ precache-manifest.5b1b387ef39959ea9a4f767e0aa7686e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029f8d49b15a9434cd7c36c1e8bee9ba",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "bfeb6a301a3b297eabfd",
+    "url": "/movie_app_2020/static/css/main.e8e27aac.chunk.css"
+  },
+  {
+    "revision": "cfdae04e43f734ca2796",
+    "url": "/movie_app_2020/static/js/2.01254aeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.01254aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfeb6a301a3b297eabfd",
+    "url": "/movie_app_2020/static/js/main.0bb64b35.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
